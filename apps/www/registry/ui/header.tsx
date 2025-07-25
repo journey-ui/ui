17218016b42b9a654a8b7bf5ca664d2933ef588a
Larissa Azevedo <larissa.sazevedo@totvspartners.com.br>
2025-07-25 14:57:11 -0300

    refactor: remove unnecessary code from header component

--- apps/www/registry/ui/header.tsx
+++ apps/www/registry/ui/header.tsx
@@ -1,68 +1,31 @@
 "use client";
-import Link from "next/link";
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 import Breadcrumbs, { Breadcrumb } from "./breadcrumbs";
-import { Button } from "./button";
-import { useBacklink } from "../lib/useBackLink";
-
-type NavigationMenuProps = {
-  links: {
-    label: string;
-    href: string;
-  }[];
-};
 
 export interface BreadcrumbsProps {
   title: string;
   description?: string;
-  image?: string;
   breadcrumbs?: Array<Breadcrumb>;
-  isLoading?: boolean;
-  returnButtonLabel?: string;
-  navigationMenu?: NavigationMenuProps;
 }
 
-const Header = ({
-  title,
-  breadcrumbs,
-  description,
-  image,
-  isLoading = false,
-  returnButtonLabel,
-  navigationMenu,
-}: BreadcrumbsProps) => {
-  const backLink = useBacklink(1);
-  const hasNavigationMenu = (navigationMenu?.links?.length ?? 0) > 0;
-
+const Header = ({ title, breadcrumbs, description }: BreadcrumbsProps) => {
   return (
-    <div
-      className={cn(
-        "relative bg-grey-2 overflow-hidden pl-18",
-        image ? "h-[259px]" : hasNavigationMenu ? "h-[259px]" : "h-[200px]"
-      )}
-    >
-      {image ? (
-        <div
-          className="absolute inset-0 bg-contain bg-no-repeat bg-right w-[840px] ml-auto container"
-          style={{ backgroundImage: `url(${image})` }}
+    <div className={cn("relative bg-grey-2 overflow-hidden pl-18 h-[200px]")}>
+      <div
+        className={cn(
+          "absolute bg-contain bg-no-repeat bg-right ml-auto container inset-0 h-[200px] -top-[18px]"
+        )}
+      >
+        <Image
+          src="/r/ui/HeaderImg.svg"
+          width={262}
+          height={258}
+          alt="Header background"
+          className="absolute inset-0 bg-contain bg-no-repeat bg-right mr-14 ml-auto right-[24px] -top-[18px] "
         />
-      ) : (
-        <div
-          className={cn(
-            "absolute bg-contain bg-no-repeat bg-right ml-auto container inset-0 h-[200px] -top-[18px]"
-          )}
-        >
-          <Image
-            src="/r/ui/HeaderImg.svg"
-            width={262}
-            height={258}
-            alt="Header background"
-            className="absolute inset-0 bg-contain bg-no-repeat bg-right mr-14 ml-auto right-[24px] -top-[18px] "
-          />
-        </div>
-      )}
+      </div>
       <title>{title}</title>
 
       <div
@@ -82,27 +45,19 @@ const Header = ({
               "data-[loading=true]:animate-pulse data-[loading=true]:rounded-sm",
               "data-[loading=true]:w-[45%] data-[loading=true]:h-12"
             )}
-            data-loading={isLoading}
           >
             {title}
           </h1>
           {description && (
             <p
               className={cn(
-                "font-semibold text-base text-grey-4 max-w-[545px]",
+                "font-semibold text-base text-grey-4 max-w-[545px]"
               )}
             >
               {description}
             </p>
           )}
         </div>
-
-        {returnButtonLabel && (
-          <Link href={backLink}>
-            <Button
-              className="font-black text-sm h-[46px] w-[202px]">{returnButtonLabel}</Button>
-          </Link>
-        )}
       </div>
     </div>
   );
