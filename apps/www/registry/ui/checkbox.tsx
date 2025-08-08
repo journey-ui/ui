744f2d45b1f9facdd2a1ae300b32abeac7284ee2
jeangregolon <jean.gregolon@totvspartners.com.br>
2025-08-08 14:49:00 -0300

    Added onCheckedChange prop to checkbox

--- apps/www/registry/ui/checkbox.tsx
+++ apps/www/registry/ui/checkbox.tsx
@@ -1,37 +1,50 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 
-import { cn } from "@/registry/lib/utils"
-import { Typography } from "@/registry/ui/typography"
-import { cva, type VariantProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority";
+import { Typography } from "./typography";
+import { cn } from "../lib/utils";
 
 const checkboxVariants = cva(
   "rounded-[4px] border-2 border-grey-5 bg-white data-[state=checked]:bg-primary data-[state=checked]:text-primary-white",
   {
     variants: {
       size: {
-        base: 'size-6',
-        sm: 'size-5',
+        base: "size-6",
+        sm: "size-5",
       },
     },
     defaultVariants: {
-      size: 'base',
+      size: "base",
     },
   }
-)
+);
 
-export interface CheckboxProps extends React.ComponentProps<typeof CheckboxPrimitive.Root>, VariantProps<typeof checkboxVariants> {
-  label?: string
+export interface CheckboxProps
+  extends Omit<
+      React.ComponentProps<typeof CheckboxPrimitive.Root>,
+      "onChange" | "onCheckedChange"
+    >,
+    VariantProps<typeof checkboxVariants> {
+  label?: string;
+  onCheckedChange?: (checked: boolean) => void;
 }
 
 function Checkbox({
   className,
   size,
   label,
+  onCheckedChange,
   ...props
 }: CheckboxProps) {
+  const handleCheckedChange = (checked: boolean | "indeterminate") => {
+    if (onCheckedChange && typeof checked === "boolean") {
+      onCheckedChange(checked);
+    }
+  };
+
   return (
     <label className="w-min [&>*]:cursor-pointer whitespace-nowrap flex items-center gap-2">
       <CheckboxPrimitive.Root
@@ -41,21 +54,30 @@ function Checkbox({
           "hover:ring-2 hover:ring-primary-2",
           className
         )}
+        onCheckedChange={handleCheckedChange}
         {...props}
       >
         <CheckboxPrimitive.Indicator
           data-slot="checkbox-indicator"
           className="flex items-center justify-center text-current transition-none"
         >
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 8.82608L6.55114 13L14 5.60491L12.3864 4L6.55114 9.80151L3.60227 6.99433L2 8.82608Z" fill="white" />
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 8.82608L6.55114 13L14 5.60491L12.3864 4L6.55114 9.80151L3.60227 6.99433L2 8.82608Z"
+              fill="white"
+            />
           </svg>
-
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
-      {!!label && (<Typography variant="p2">{label}</Typography>)}
+      {!!label && <Typography variant="p2">{label}</Typography>}
     </label>
-  )
+  );
 }
 
-export { Checkbox, checkboxVariants }
+export { Checkbox, checkboxVariants };
