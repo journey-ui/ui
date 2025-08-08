b98988f2f40ba90470ea027edac241fddbc59d42
jeangregolon <jean.gregolon@totvspartners.com.br>
2025-08-08 15:04:22 -0300

    Added doc page to sidebar

--- apps/www/components/sidebar.tsx
+++ apps/www/components/sidebar.tsx
@@ -16,78 +16,83 @@ export function Sidebar() {
       items: [
         {
           label: "Inicio rápido",
-          href: "/guias/inicio-rapido"
-        }
-      ]
+          href: "/guias/inicio-rapido",
+        },
+      ],
     },
     {
       label: "Blocos",
       items: [
         {
           label: "Header",
-          href: "/blocks/header"
-        }
-      ]
+          href: "/blocks/header",
+        },
+      ],
     },
     {
       label: "Componentes",
       items: [
         {
           label: "Typography",
-          href: "/componentes/typography"
+          href: "/componentes/typography",
         },
         {
           label: "Button",
-          href: "/componentes/button"
+          href: "/componentes/button",
         },
         {
           label: "Badge",
-          href: "/componentes/badge"
+          href: "/componentes/badge",
         },
         {
           label: "Tooltip",
-          href: "/componentes/tooltip"
+          href: "/componentes/tooltip",
         },
         {
           label: "Breadcrumbs",
-          href: "/componentes/breadcrumbs"
+          href: "/componentes/breadcrumbs",
         },
         {
           label: "File Uploader",
-          href: "/componentes/file-uploader"
+          href: "/componentes/file-uploader",
         },
         {
           label: "Dropdown",
-          href: "/componentes/dropdown"
+          href: "/componentes/dropdown",
         },
         {
           label: "Dropdown menu",
-          href: "/componentes/dropdown-menu"
+          href: "/componentes/dropdown-menu",
         },
         {
           label: "Checkbox",
-          href: "/componentes/checkbox"
-        }
-      ]
-    }
-  ]
+          href: "/componentes/checkbox",
+        },
+        {
+          label: "Dialog",
+          href: "/componentes/dialog",
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="w-56 flex-shrink-0 h-full overflow-y-auto py-8 space-y-8">
       {navigations.map((navigation, index) => (
         <div key={index} className="flex flex-col gap-2 mr-16">
-          <Typography variant="p1b" className="leading-none px-2">{navigation.label}</Typography>
+          <Typography variant="p1b" className="leading-none px-2">
+            {navigation.label}
+          </Typography>
 
           <nav>
             <ul>
-              {navigation.items.sort((a, b) => a.label.localeCompare(b.label)).map((item, indexItem) => (
-                <li key={indexItem}>
-                  <LinkSidebar
-                    href={item.href}
-                    label={item.label}
-                  />
-                </li>
-              ))}
+              {navigation.items
+                .sort((a, b) => a.label.localeCompare(b.label))
+                .map((item, indexItem) => (
+                  <li key={indexItem}>
+                    <LinkSidebar href={item.href} label={item.label} />
+                  </li>
+                ))}
             </ul>
           </nav>
         </div>
@@ -104,7 +109,12 @@ interface LinkSidebarProps {
 function LinkSidebar({ href, label }: LinkSidebarProps) {
   return (
     <Link href={href}>
-      <Typography variant="p2" className="leading-none w-min whitespace-nowrap p-2 rounded-md hover:bg-grey-3">{label}</Typography>
+      <Typography
+        variant="p2"
+        className="leading-none w-min whitespace-nowrap p-2 rounded-md hover:bg-grey-3"
+      >
+        {label}
+      </Typography>
     </Link>
-  )
+  );
 }
